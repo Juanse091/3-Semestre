9ed8e7f4abbd90067a451e1059c087685fbbc38f
Shaj <88673999+Juanse091@users.noreply.github.com>
2022-08-24 11:27:14 -0500

    Add files via upload

---
+++ Ejc.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 24 11:19:36 2022
+
+@author: admin
+"""
+
+x = int(input("Inicio: "))
+a = int(input("Multiplicador: "))
+c = int(input("Desface: "))
